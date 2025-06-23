4d4cde29f7b176a3779737c5db05a07f1b26981f
Awais-Maqbool <awaismaqbool579@gmail.com>
2025-06-23 15:24:03 +0000

    js

---
+++ basic/variables.js
@@ -0,0 +1,15 @@
+const id = 1234;
+let email = "awais";
+var pass = "5678";
+city = "lahore";
+let zain;
+
+//id = 4567;
+email = "maqbool";
+pass = "1234";
+city = "layyah";
+
+console.log(email);
+console.log(pass)
+
+//console.table([email,pass,city,zain]);
