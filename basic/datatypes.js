2ce23c1ac350d7b4f681e5a00e4b5ef417d18f07
Awais-Maqbool <awaismaqbool579@gmail.com>
2025-06-23 16:59:26 +0000

    js

--- basic/datatypes.js
+++ basic/datatypes.js
@@ -2,4 +2,10 @@ let name = "awais";
 let age = 12;
 let eatfood = true;
 
-console.table([name,age,eatfood])
+console.table([name,age,eatfood]);
+
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof eatfood);
+console.log(typeof undefined); //undefined shows typeof undefined
+console.log(typeof null); //but null shows the type of object
