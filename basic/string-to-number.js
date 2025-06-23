d8fe0c239f571a5fdad7e77a253fa0e6fbcd85de
Awais-Maqbool <awaismaqbool579@gmail.com>
2025-06-23 17:53:18 +0000

    js

---
+++ basic/string-to-number.js
@@ -0,0 +1,24 @@
+let za = "33abc";
+let ta = Number(za);
+//console.log(ta)
+//console.log(typeof ta)
+
+/*console.log(2+2)
+console.log(2-2)
+console.log(2*2)
+console.log(2**2)
+console.log(2/2)
+console.log(2%2)*/
+
+let str1 = "hello";
+let str2 = " awais";
+//console.log(str1.concat(str2))
+let str3 = str1.concat(str2);
+//console.log(str3)
+//console.log(typeof str3)
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 3);
+console.log(1 + 2 + "3");
+
