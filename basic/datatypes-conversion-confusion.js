68c6da1acd008a85bf31bd09857d85f0a179bfcb
Awais-Maqbool <awaismaqbool579@gmail.com>
2025-06-23 17:21:09 +0000

    js

---
+++ basic/datatypes-conversion-confusion.js
@@ -0,0 +1,27 @@
+//let score = 33;
+let score = false
+
+//console.log(typeof score);
+
+let value = Number(score);  //score ki type ko number bnana kaliya
+//let value = String(score)
+
+//console.log(typeof value);
+//console.log(value)
+
+//"33" => 33
+//"33abc" => nan;
+//null => 0;
+//undefined => nan;
+//true => 1;
+//false => 0;
+//"awais"=>nan
+
+let check = 1;
+let dbcheck = Boolean(check);
+//console.log(dbcheck)
+
+//1=>true and 0=>false
+//""=>false and "awais" = true
+
+
