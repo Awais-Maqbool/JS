1d0b04a3c93750fa079935656d2f019760c853ec
Awais-Maqbool <awaismaqbool579@gmail.com>
2025-06-26 07:13:59 +0000

    js

---
+++ basic/stack-heap-memory.js
@@ -0,0 +1,16 @@
+let name = "awais";
+let anothername = name;
+console.log(name);
+
+anothername="bilal";
+console.log(anothername);
+
+let userone =  {
+    name:"awais",
+}
+let usertwo = userone;
+
+usertwo.name = "bilal";
+
+console.log(userone);
+console.log(userone);
