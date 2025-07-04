4e92650db12d5347390039c0e015d6d83f0fa6c2
Awais-Maqbool <awaismaqbool579@gmail.com>
2025-07-04 07:09:47 +0000

    js

---
+++ basic/String.js
@@ -0,0 +1,24 @@
+let z = "awais";
+let y = "bilal";
+
+
+console.log(z[0])
+
+const game = new String("pubg");
+console.log(game.__proto__);
+console.log(game.length);
+console.log(game.toUpperCase());
+console.log(game.toLowerCase());
+console.log(game.concat(y));
+console.log(game.charAt(1));
+console.log(game.split(2,2));
+console.log(game.replace("p","f"))
+console.log(game.substring(2))
+console.log(game.indexOf('u'))
+
+console.log(game)
+
+let string = "  awais  ";
+console.log(string.trimEnd())
+
+console.log(game.includes("aw"));
