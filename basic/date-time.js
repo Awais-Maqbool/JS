bbb19376275c2ae6d54fab2ccd8adcb6116fe05f
Awais-Maqbool <awaismaqbool579@gmail.com>
2025-07-05 07:59:21 +0000

    js

---
+++ basic/date-time.js
@@ -0,0 +1,22 @@
+// let date = new Date();
+// console.log(date.toString());
+// console.log(date);
+// console.log(date.toLocaleString());
+// console.log(date.toDateString())
+
+let newdate = new Date("2023-01-12");
+// console.log(newdate.toDateString());
+// console.log(newdate.toLocaleString());
+// console.log(newdate.toLocaleDateString());
+
+console.log(newdate.getDate()+1);
+console.log(newdate.getMonth()+1);
+
+let timestamp = Date.now();
+// console.log(timestamp);
+// console.log(newdate.getTime());
+// console.log(timestamp - newdate.getTime())
+
+//console.log(Math.floor((Date.now())/1000))
+
+
