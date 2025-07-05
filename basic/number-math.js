4ad7137958bff75eccdf6977c6514857a981d78c
Awais-Maqbool <awaismaqbool579@gmail.com>
2025-07-05 07:21:42 +0000

    js

---
+++ basic/number-math.js
@@ -0,0 +1,47 @@
+// const numb = new Number(100);
+// console.log(numb);
+// const y = numb.toString();
+// const x = numb.toFixed(2);
+// const c = numb.toPrecision(5);
+// const d = numb.toExponential(5);
+
+// console.log(x);
+// console.log(y);
+// console.log(c);
+// console.log(d);
+// console.log(typeof y)
+// console.log(typeof x)
+
+
+// const score = new Number(10000000000);
+// console.log(score.toLocaleString());
+
+
+// +++++++++++++++++++++++++++++
+
+
+// console.log(Math);
+
+// console.log(Math.abs(-6));
+
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.3));
+// console.log(Math.floor(4.9));
+
+// console.log(Math.sqrt(25));
+// console.log(Math.sqrt(24));
+
+// console.log(Math.pow(2,5))
+
+// console.log(Math.min(1,2,3,4,5))
+// console.log(Math.max(1,2,3,4,5))
+
+// console.log(Math.random());
+// console.log((Math.random()*10)+1);
+
+
+const min = 10;
+const max = 20;
+console.log(Math.floor(Math.random() * (max-min + 1))+ min);
+
+
