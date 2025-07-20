e508c161fa9d9bdde7a2d820354d937c4bc12708
Awais-Maqbool <awaismaqbool579@gmail.com>
2025-07-20 11:11:58 +0000

    js

---
+++ basic/global-localscope.js
@@ -0,0 +1,53 @@
+var c = 300;
+
+if(true){
+    let a = 10;
+const b = 20;
+ c = 30;
+}
+console.log(c)
+
+// function one(){
+//     let name = "awais";
+//     function two(){
+//         let age = 12;
+//         console.log(name)
+//     }
+//     //console.log(age)
+
+//     two()
+// }
+// one()
+
+// if(true){
+//     let name = 'awais';
+//     if(true){
+//         let age = 12;
+//         console.log(name)
+//     }
+//    // console.log(age)
+// }
+// //console.log(name)
+
+
+// +++++++++++++++++ HOISTING ++++++++++++++
+
+add(5)
+
+function add(n){
+    return n+1
+}
+
+
+
+// const result = function (n){
+//     return n+1                       //it is correct
+// }
+// result(5)
+
+
+// result1(5)
+
+// const result1 = function (n){   ///throw error
+//     return n+1
+// }
