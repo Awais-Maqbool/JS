cb060aec04e892c2eaa7fb50366a9c722536388f
Awais-Maqbool <awaismaqbool579@gmail.com>
2025-07-21 13:05:12 +0000

    js

---
+++ basic/this-arrowfunc.js
@@ -0,0 +1,40 @@
+// // let z = {
+// //     name:"awais",
+// //     age:12,
+// //     func:function (){
+// //         console.log(`my name is ${this.name}`)
+// //         console.log(this)
+// //     }
+// // }
+// // z.func()
+// // console.log(this)
+
+// // function z(){
+// //     let y = "awais"
+// //     console.log(this)    //this is not working in functions
+// // }
+// // z()
+
+// // let chai = ()=>{
+// //     let y = "awais"
+// //     console.log(this)  
+// // }
+// // chai()
+
+// // let add = (a,b)=>{
+// //     return a+b 
+// // }
+// // let c = add(5,5)
+// // console.log(c)
+
+// // let add = (a,b) => (a+b)
+// // console.log(add(5,6))
+
+// let add = (a,b)=>({name:"awais"})
+// console.log(add(4,5))
+
+
+
+
+
+
