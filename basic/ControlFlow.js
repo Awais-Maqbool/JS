f685e9e94f01aeb47e3ee8597df5c7e4d80d1524
Awais-Maqbool <awaismaqbool579@gmail.com>
2025-07-25 09:54:59 +0000

    js

---
+++ basic/ControlFlow.js
@@ -0,0 +1,84 @@
+// // // 3!=2;
+// // if(3!=3){
+// //     console.log("hlo")
+// // }
+
+//  let score = 100;
+// // if(score>50){
+// //     const power = "fly"
+// //     console.log(`user is ${power}`)
+// // }
+
+// // if(score>50) console.log("hlo");  //implicit scope
+
+// // let balance = 1000;
+// // if(balance<500){
+// //     console.log("yes")
+// // }
+// // else if(balance<900){
+// //     console.log("yes")
+// // }
+// // else{
+// //     console.log("yes i ahve")
+// // }
+
+// // let userloggedin = true;
+// // let debitcard = true;
+// // let loginfromemail = true;
+// // let loginfromgoogle=false;
+
+// // // if(userloggedin && debitcard && 2=="2"){
+// // //     console.log("hlo yes you can buy this course")
+// // // }
+// // if(loginfromemail || loginfromgoogle){
+// //     console.log("hlo yes you can buy this course")
+// // }
+
+// let day = 7;
+// switch(day){
+//     case 1:
+//         console.log("sunday");
+//         break;
+        
+//     case 2:
+//         console.log("monday");
+//         break;
+        
+//     case 3:
+//         console.log("tuesday");
+//         break;
+        
+//     case 4:
+//         console.log("wednesday");
+//         break;
+        
+//     default:
+//         console.log("nothing")
+// }
+
+// let z = 0n;
+// if(z){
+//     console.log("awais");
+// }
+// else{
+//     console.log("not awais");
+// }
+
+// let z = [];
+// if(z.length==0){
+//     console.log('yes');
+    
+// }
+
+// let z = {name:"awais,"};
+// if(Object.keys(z).length=="1"){
+//     console.log('yes')
+// }
+// else{
+//     console.log("not empty");
+    
+// }
+
+// let price = 100;
+// price<=80 ? console.log("less than 80") : console.log("more than 80")
+
