eec8bedbf65e97a6201b329a52b62b126e1a544d
Awais-Maqbool <awaismaqbool579@gmail.com>
2025-07-25 10:23:58 +0000

    js

---
+++ basic/for-loop-break-continue.js
@@ -0,0 +1,17 @@
+// for (let i = 0; i < 10; i++) {
+//     console.log(i)    
+// }
+
+// for(let i=1;i<=10;i++){
+//     console.log(`Outer loop : ${i}`)
+//     for(let j=1;j<=10;j++){
+//         console.log(i + "*" + j + "=" + i*j)
+//     }
+// }
+
+// for(let i=1;i<=10;i++){
+//     if(i==5){
+//         continue;
+//     }
+//     console.log(i)
+// }
