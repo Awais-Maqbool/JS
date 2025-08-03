0e64c6593df9de57be19b25f698cafe0b90648d0
Awais-Maqbool <awaismaqbool579@gmail.com>
2025-08-03 10:55:27 +0000

    js

---
+++ basic/Dom-selection.js
@@ -0,0 +1,104 @@
+// <!DOCTYPE html>
+// <html lang="en">
+// <head>
+//     <meta charset="UTF-8">
+//     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+//     <title>Dom</title>
+// </head>
+// <body>
+//      <!-- <div class="black">
+//         <h1 id="title" class="heading">js from chai aur code</h1>
+//         <h2>Lorem, ipsum dolor 1.</h2> -->
+//         <!-- <h2>Lorem, ipsum dolor 2.</h2> -->
+//         <!-- <h2>Lorem, ipsum dolor 3</h2>
+//         <p class="para" >Lorem ipsum dolor <span style="display: none;">sit</span> amet.</p>
+//         <p class="para">awais</p>
+//         <p class="para">awais maqbool</p>
+
+//         <input type="password" name="" id="">
+
+//         <ul>
+//             <li>one </li>
+//             <li>one </li>
+//             <li>one </li>
+//             <li>one </li>
+//         </ul>
+//     </div>  -->
+
+//     <div class="parent">
+//         <!-- this is cmnt -->
+//         <div class="day">monday</div>
+//         <div class="day">tuesday</div>
+//         <div class="day">wednesday</div>
+//         <div class="day">thursday</div>
+//     </div>
+// </body>
+// <script>
+//     let parent = document.querySelector(".parent")
+    
+   
+//     console.log(parent)
+//     console.log(parent.children[3])
+
+//     // let l = Array.from(parent.children)
+//     // l.forEach((child)=>{
+//     //     child.style.color="red"
+//     // })
+
+//     // for(let i=0;i<parent.children.length;i++){
+//     //     // console.log(parent.children[i])
+//     //     parent.children[i].style.color="yellow"
+//     // }
+
+//     // let d1=document.querySelector(".day")
+//     // console.log(d1.parentElement)
+//     // console.log(d1.nextElementSibling)
+//     // console.log(d1.previousElementSibling)
+//     // console.log(d1.nextSibling)
+
+//     console.log("nodes:", parent.childNodes)
+    
+// </script>
+// </html>
+
+
+// <!-- document.getElementById("title").className
+// 'heading'
+// document.getElementById("title").id
+// 'title'
+// document.getElementById("title");
+// document.getElementById("title").getAttribute("id")
+// 'title'
+// document.getElementById("title").setAttribute("class","test heading")
+// title.style.backgroundColor="green"
+// 'green'
+// title.textContent
+// 'js from chai aur code'
+// title.innerText
+// 'js from chai aur code'
+// title.innerHTML
+// 'js from chai aur code' -->
+
+// <!-- document.querySelector("h2)
+// VM5240:1 Uncaught SyntaxError: Invalid or unexpected token
+// document.querySelector("h2")
+// <h2>​Lorem, ipsum dolor.​</h2>​
+// document.querySelector("#title")
+// <h1 id=​"title" class=​"heading">​js from chai aur code​</h1>​
+// document.querySelector(".heading") -->
+
+// <!-- document.querySelectorAll("p")
+// NodeList(3) [p.para, p.para, p.para]0: p.para1: p.para2: p.paralength: 3[[Prototype]]: NodeList
+// document.querySelectorAll("title)
+// VM5879:1 Uncaught SyntaxError: Invalid or unexpected token
+// document.querySelectorAll("title")
+// NodeList [title]0: titlelength: 1[[Prototype]]: NodeList
+// document.querySelector("title")
+// <title>​Dom​</title>​
+// document.querySelector("#title")
+// <h1 id=​"title" class=​"heading">​js from chai aur code​</h1>​
+// document.querySelector(".heading")
+// <h1 id=​"title" class=​"heading">​js from chai aur code​</h1>​
+// document.querySelector(".para")
+// <p class=​"para">​…​</p>​"Lorem ipsum dolor "<span style=​"display:​ none;​">​sit​</span>​" amet."</p>​
+// document.querySelectorAll(".para") --></html>
