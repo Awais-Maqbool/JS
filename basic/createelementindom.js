259567f580cdd17fe217d1613f8bcfbc4050ea8b
Awais-Maqbool <awaismaqbool579@gmail.com>
2025-08-03 11:15:57 +0000

    js

---
+++ basic/createelementindom.js
@@ -0,0 +1,51 @@
+// <!DOCTYPE html>
+// <html lang="en">
+// <head>
+//     <meta charset="UTF-8">
+//     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+//     <title>dom</title>
+//     <style>
+//         body{
+//             background-color: #414141;
+//             color: #fff;
+//         }
+//     </style>
+// </head>
+// <body>
+    
+// </body>
+// <script>
+//     // let div = document.createElement("div")
+//     // console.log(div)
+//     // div.className="main"
+//     // div.id=Math.floor(Math.random()*10+1)
+//     // div.setAttribute("title","awais maqbool")
+//     // div.style.backgroundColor="red"
+//     // div.style.padding="12px"
+//     // div.setAttribute("h1","hlo my name is awais")
+//     // // div.innerText="Chai aur code"
+//     // let text = document.createTextNode("chai aur code")
+//     // div.appendChild(text)
+//     // document.body.appendChild(div)
+
+
+
+
+
+
+
+
+//     let h1 = document.createElement("h1")
+//     console.log(h1)
+//     h1.id="awais"
+//     h1.className="maqbool"
+//     h1.setAttribute("title","chai aur code")
+//     h1.style.backgroundColor="green"
+//     // h1.innerHTML="my name is awais"
+//     let text = document.createTextNode("my name is awais")
+//     h1.appendChild(text)
+//     document.body.appendChild(h1)
+
+
+// </script>
+// </html>
