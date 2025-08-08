f656a89cbea4a380309dc0ed5da89b775296a3a9
Awais-Maqbool <awaismaqbool579@gmail.com>
2025-08-08 11:42:23 +0000

    js

---
+++ basic/timeinterval.js
@@ -0,0 +1,83 @@
+// <!DOCTYPE html>
+// <html lang="en">
+// <head>
+//     <meta charset="UTF-8">
+//     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+//     <title>awais</title>
+//     <style>
+//         body{
+//             background-color: #414141;
+//             color: #fff;
+//         }
+//     </style>
+// </head>
+// <body>
+//     <h1>awais Maqboool</h1>
+//     <button id="start">Start</button>
+//     <button id="stop">Stop</button>
+// </body>
+// <script>
+
+
+//     // // let show = ()=>{
+//     // //     console.log("awais")
+//     // // }
+//     // // setTimeout(show,2000)
+
+//     // let change = ()=>{
+//     //     document.querySelector("h1").innerHTML = "stop"
+//     // }
+//     // let awais = setTimeout(change,2000)
+
+//     // document.querySelector("#btn").addEventListener("click",(e)=>{
+//     //     clearTimeout(awais)
+//     //     console.log("stopped");
+//     // })
+
+
+
+
+//     // setTimeout(change,2000)
+//     // let change = (()=>{
+//     //     document.querySelector("h1").innerHTML="hussain"
+//     // })
+    
+
+//     // document.querySelector("#start").addEventListener('click',(e)=>{
+        
+//     // let z = setTimeout(change,2000)
+
+//     //  document.querySelector("#stop").addEventListener("click",(e)=>{
+//     //     clearTimeout(z)
+//     //     console.log("stopped");
+        
+//     // })
+         
+//     // })
+
+
+//     // ++++++++++++++++++++++Set intervel++++++++=
+
+//     // let s = (()=>{
+//     //     console.log("awais" , Date.now())
+//     // })
+
+//     // document.querySelector("#start").addEventListener("click",(e)=>{
+        
+//     // let z = setInterval(s,2000)
+
+//     // document.querySelector("#stop").addEventListener("click",(e)=>{
+//     //     clearInterval(z)
+//     //     console.log("stopped")
+//     // })
+//     // })
+    
+    
+
+    
+   
+
+     
+
+// </script>
+// </html>
