c84fc9454ae15a535b85455739029254feb7c437
Awais-Maqbool <awaismaqbool579@gmail.com>
2025-08-08 07:38:03 +0000

    js

---
+++ basic/events.js
@@ -0,0 +1,112 @@
+// <!DOCTYPE html>
+// <html lang="en">
+// <head>
+//   <meta charset="UTF-8">
+//   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+//   <title>Html</title>
+//   <style>
+//     body{
+//       background-color: #414141;
+//       color: #fff;
+//     }
+//   </style>
+// </head>
+// <body>
+//     <h2>Amazing images</h2>
+//     <ul id="images">
+//       <li id="cycle"><img style="width: 200px; height: 200px; object-fit: contain;" src="pexels-damir-33209986.jpg" alt=""></li>
+//       <li id="persons"><img style="width: 200px; height: 200px; object-fit: contain;" src="pexels-enesersahin-29529439.jpg" alt=""></li>
+//       <li id="awais"><img style="width: 200px; height: 200px; object-fit: contain;" src="WhatsApp Image 2025-08-03 at 10.49.59 AM.jpeg" alt=""></li>
+//       <a id="google" style = "color: #fff;" href="https://google.com">google.com</a>
+//     </ul>
+// </body>
+// <script>
+//   // let awais = document.querySelector("#awais").onclick=function(){
+//   //   alert("awais")
+//   // }
+
+//   // timestamp,preventdefault,type,
+//   // target,toelement,srcelement,currentelement, 
+//   // client x,client y,screen x,screen y
+//   // altkey,ctrlkey,shiftkey.keycode
+//   //  let img = document.getElementById("images")
+//   // img.addEventListener("click",(e)=>{
+//   //   console.log("clicked the url")
+//   // })
+
+//   // let awais = document.querySelector("#awais")
+//   // awais.addEventListener("click",(e)=>{
+//   //   console.log('clicked awais');
+//   //   e.stopPropagation()
+//   //   e.style.display = "block"
+//   //   // alert('awais')
+//   // })
+
+//   // let goo = document.getElementById("google")
+//   // goo.addEventListener("click",(e)=>{
+//   //   console.log("google clicked")
+//   //   e.preventDefault()
+//   //   e.stopPropagation()
+//   // })
+
+//   // document.querySelector("#images").addEventListener("click",(e)=>{
+//   //   // console.log(e.target.parentNode)
+//   //   // console.log(e.target.tagName)
+    
+//   //   if(e.target.tagName==="IMG" || e.target.tagName ==="A"){
+//   //     // let remove = e.target.parentNode
+
+//   //     // let remove = e.target.parentNode
+//   //     // remove.style.display = "none"        for removing an elements
+
+
+//   //     // remove.remove()
+
+//   //     e.preventDefault()
+//   //   }
+//   //   console.log(remove)
+    
+    
+//     // remove.remove()
+//   // })
+
+//   // document.querySelector("#images").addEventListener("click",(e)=>{
+//   //   console.log(("unordered list clicked"));
+//   // },false)
+//   // document.querySelector("#awais").addEventListener("click",(e)=>{
+//   //   console.log(("awais clicked"));
+//   // },false)
+
+//   // document.querySelector("#images").addEventListener("click",(e)=>{
+//   //   // console.log(e.target.parentNode);
+    
+//   //   if(e.target.tagName==='IMG'){
+//   //     console.log(e.target.parentNode);
+      
+//   //     let remove = e.target.parentNode
+//   //     remove.remove()
+//   //   }
+    
+    
+//   //   e.preventDefault()
+//   // })
+
+
+  
+ 
+// </script>
+// </html>
+
+
+
+// <!DOCTYPE html>
+// <html lang="en">
+// <head>
+//   <meta charset="UTF-8">
+//   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+//   <title>Document</title>
+// </head>
+// <body>
+  
+// </body>
+// </html>
