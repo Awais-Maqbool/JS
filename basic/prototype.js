f5fbbdc0e1b8380de951139599a390c69144a237
Awais-Maqbool <awaismaqbool579@gmail.com>
2025-08-12 11:21:22 +0000

    js

---
+++ basic/prototype.js
@@ -0,0 +1,66 @@
+// function m(num){
+//     return num*5
+// }
+// console.log(m(5))
+// m.power=2
+// console.log(m.power)
+// console.log(m.prototype)
+
+
+// function createuser(username,score){
+//     this.username=username
+//     this.score=score
+
+//     // return this
+// }
+
+// createuser.prototype.increment = function(){
+//     this.score++
+// }
+
+// createuser.prototype.printme = function(){
+//     console.log(`the price is ${this.score}`);
+    
+// }
+
+// let chai =  new createuser("chai",25)
+// let tea =  new createuser("tea",250)
+
+// chai.printme()
+// tea.printme()
+// chai.increment()
+// chai.printme()
+// // console.log(chai);
+
+// +++++++++++++++++++++++++++++++++++++++protoype+++++++++++
+
+// let heros = ["thor","spiderman"]
+ 
+// let heropower = {
+//     thor:"hammer",
+//     spiderman:"sling",
+
+//     getspiderpower : function(){
+//         console.log(`spidy power is ${this.spiderman}`);
+        
+//     }
+// }
+
+// Object.prototype.awais=function(){
+//     console.log(`awais is present in all objects`);
+// }
+
+// Array.prototype.heyawais = function(){
+//     console.log("hey awais");
+// }
+
+// heropower.awais()
+// heros.awais()
+// heros.heyawais()
+// heropower.heyawais()
+
+
+
+
+
+
