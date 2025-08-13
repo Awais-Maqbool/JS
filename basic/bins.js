089bedcbe8dc99a139bfb29def9c47954179f6b0
Awais-Maqbool <awaismaqbool579@gmail.com>
2025-08-13 04:11:04 +0000

    js

---
+++ basic/bins.js
@@ -0,0 +1,31 @@
+// <!DOCTYPE html>
+// <html lang="en">
+// <head>
+//   <meta charset="UTF-8">
+//   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+//   <title>Document</title>
+//   <style>
+//     body{
+//       background-color: #414141;
+//       color: #fff;
+//     }
+//   </style>
+// </head>
+// <body>
+//     <button>clicked me</button>
+// </body>
+//   <script>
+//     class react{
+//       constructor(){
+//         this.server = "server"
+
+//         document.querySelector('button').addEventListener('click',this.clickme.bind(this))
+//       }
+//       clickme(){
+//         console.log("button clicked");
+//         console.log(this.server);
+//       }
+//     }
+//       let chai = new react()
+//   </script>
+// </html>
